29a895f41bc32578f884d4cd6d6c8375d924956a
ruben <rubenpedrosaoliveira@gmail.com>
2019-09-02 20:02:35 +0200

    Sort and select by number of torrent seeds

--- deluge_torrent_adder.py
+++ deluge_torrent_adder.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 # Import the client module
+import json
+
+import bs4
 from deluge.ui.client import client
 # Import the reactor module from Twisted - this is for our mainloop
 from twisted.internet import reactor
+import requests
 import pickle
+import re
 import os
 import logging.config
 
@@ -59,8 +64,8 @@ def on_connect_success(result):
 
     torrents_to_add = get_torrents_to_add()
     for torrent in torrents_to_add:
-        client.core.add_torrent_magnet(torrent['magnet'], {}).addCallback(on_torrent_added,torrent['title'])\
-                                                                 .addErrback(on_torrent_added_fail)
+        client.core.add_torrent_magnet(torrent['magnet'], {}).addCallback(on_torrent_added, torrent['title']) \
+            .addErrback(on_torrent_added_fail)
     if os.path.isfile(pickle_file_name):
         os.remove(pickle_file_name)
         LOGGER.info("Torrents pickle file removed")
@@ -73,11 +78,12 @@ def get_torrents_to_add() -> list:
 
     magnets = []
     for k, v in torrents.items():
-        magnet_title, magnet_link = get_best_torrent_option(v)
+        new_torrent_list = get_torrents_seeds(v)
+        magnet_title, magnet_link = get_best_torrent_option(new_torrent_list)
         if magnet_title != '':
             magnets.append({"title": magnet_title, "magnet": magnet_link})
         else:
-            LOGGER.info("No torrent to add!")
+            LOGGER.info("No torrent to add for {:}".format(k))
 
     return magnets
 
@@ -87,23 +93,42 @@ def get_best_torrent_option(torrent_options: list) -> tuple:
         web_rel = [w for w in torrent_options if 'WEB'.casefold() in w.get('rip_type').casefold()]
         hdtv_rel = [h for h in torrent_options if 'HDTV'.casefold() in h.get('rip_type').casefold()]
         if len(web_rel) > 0:
-            web_rel.sort(key=lambda k: k['size'], reverse=True)
+            web_rel.sort(key=lambda k: k['seeders'], reverse=True)
             for web_t in web_rel:
                 if web_t.get('size') < 1500.0:
                     return web_t.get('title'), web_t.get('magnet')
         elif len(hdtv_rel) > 0:
-            hdtv_rel.sort(key=lambda k: k['size'])
+            hdtv_rel.sort(key=lambda k: k['seeders'])
             for hd_t in hdtv_rel:
                 if hd_t.get('size') < 1500.0:
                     return hd_t.get('title'), hd_t.get('magnet')
         else:
-            torrent_options.sort(key=lambda k: k['size'], reverse=True)
+            torrent_options.sort(key=lambda k: k['seeders'], reverse=True)
             for opt in torrent_options:
                 if opt.get('size') < 1500.0:
                     return opt.get('title'), opt.get('magnet')
 
     return '', ''
 
+def get_torrents_seeds(torrent_options: list) -> list:
+    with open(os.path.join(__location__, 'rarbg_cookie.json')) as cookie:
+        rarbg_cookie = json.load(cookie)
+    if len(torrent_options) > 0:
+        new_torrent_options = []
+        for torrent in torrent_options:
+            get_torrent = requests.get(torrent['link'], cookies=rarbg_cookie)
+            soup_obj = bs4.BeautifulSoup(get_torrent.text, features="lxml")
+            table = soup_obj.select('body > table:nth-child(6) > tr > td:nth-child(2) > div > table > tr:nth-child(2) > td > div > table')
+            for tr in table[0].select('tr'):
+                for td in tr.select('td', {'class': 'header2'}):
+                    if td.text.strip() == "Peers:":
+                        peers = tr.select('td.lista')[0].text.strip()
+                        seeders = re.search('Seeders : (\d*)', peers)
+                        torrent['seeders'] = seeders.group(1)
+            new_torrent_options.append(torrent)
+        return new_torrent_options
+
+    return []
 
 # We create another callback function to be called when an error is encountered
 def on_connect_fail(result):
